bae7ae37ab162c47d929f6aef8cdf8eeaf360fd7
jalvarez <lcalvarez@unicauca.edu.co>
2022-05-16 08:26:52 -0500

    todo

---
+++ fuel/app/logs/2022/05/16.php
@@ -0,0 +1,5 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+WARNING - 2022-05-16 15:10:55 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2022-05-16 15:13:27 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2022-05-16 15:18:45 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
